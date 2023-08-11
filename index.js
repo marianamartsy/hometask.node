5e4179caca7e4f7d13708ec4af76b0ed63940dc7
Mariana Martsynyuk <123500008+marianamartsy@users.noreply.github.com>
2023-08-11 16:26:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 require("dotenv").config();
 
+
 const args = process.argv;
 const p_index = args.indexOf("--p");
 const cp_index = args.indexOf("--cp");
